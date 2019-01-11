aa463f863861952f89e95fec516909774fcc3c31
Marshall Offutt <marshalloffutt@gmail.com>
2019-01-11 17:27:55 -0600

    stuff

--- src/App/App.js
+++ src/App/App.js
@@ -2,13 +2,31 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import {
+  BrowserRouter, Route, Redirect, Switch,
+} from 'react-router-dom';
 
 import connection from '../helpers/data/connection';
 import authRequests from '../helpers/data/authRequests';
 
-import Auth from '../components/Auth/Auth';
+import Auth from '../components/pages/Auth/Auth';
+import Home from '../components/pages/Home/Home';
 import Mavbar from '../components/Mavbar/Mavbar';
 import './App.scss';
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (authed === false
+    ? (<Component { ...props } />)
+    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />));
+  return <Route {...rest} render={props => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (authed === true
+    ? (<Component { ...props } />)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />));
+  return <Route {...rest} render={props => routeChecker(props)} />;
+};
 
 class App extends React.Component {
   state = {
@@ -34,10 +52,6 @@ class App extends React.Component {
     this.removeListener();
   }
 
-  isAuthenticated = () => {
-    this.setState({ authed: true });
-  }
-
   render() {
     const { authed } = this.state;
     const logoutClicky = () => {
@@ -45,19 +59,20 @@ class App extends React.Component {
       this.setState({ authed: false });
     };
 
-    if (!authed) {
-      return (
-        <div className="App">
-          <Mavbar isAuthed={authed} logoutClicky={logoutClicky} />
-          <Auth isAuthenticated={this.isAuthenticated} />
-        </div>
-      );
-    }
-
     return (
       <div className="App">
-        <Mavbar isAuthed={authed} logoutClicky={logoutClicky} />
-        <h1>You Are Authenticated.</h1>
+        <BrowserRouter>
+          <React.Fragment>
+            <Mavbar isAuthed={authed} logoutClicky={logoutClicky} />
+            <div className="row">
+              <Switch>
+                <PrivateRoute path='/' exact component={Home} authed={this.state.authed} />
+                <PrivateRoute path='/home' component={Home} authed={this.state.authed} />
+                <PublicRoute path='/auth' component={Auth} authed={this.state.authed} />
+              </Switch>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
