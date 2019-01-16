786a9f8a6121d08bb550b11b8ea7e90dad9452b6
Marshall Offutt <marshalloffutt@gmail.com>
2019-01-15 20:53:31 -0600

    bulids new friend form

--- src/components/pages/NewFriend/NewFriend.js
+++ src/components/pages/NewFriend/NewFriend.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import {
+  Button, Form, FormGroup, Label, Input,
+} from 'reactstrap';
 import './NewFriend.scss';
 
 class NewFriend extends React.Component {
   render() {
     return (
-      <div className='NewFriend'>
-        <h3>New Friend Page</h3>
-      </div>
+      <Form>
+        <FormGroup>
+          <Label for="name">Name</Label>
+          <Input type="text" name="name" id="inputName" placeholder="Friend McFriendface" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="phone">Phone</Label>
+          <Input type="text" name="phone" id="inputPhone" placeholder="Phone number" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="email">Email</Label>
+          <Input type="email" name="email" id="email" placeholder="E-mail" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="relationship">Relationship</Label>
+          <Input type="text" name="relationship" id="relationshipInput" placeholder="hum aapke hain koun..!" />
+        </FormGroup>
+        <FormGroup check>
+          <Label check>
+            <Input type="checkbox" />{' '}
+            Are you avoiding this person?
+          </Label>
+        </FormGroup>
+        <Button color="primary">Submit</Button>
+      </Form>
     );
   }
 }
